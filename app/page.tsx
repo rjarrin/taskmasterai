12fb56bbf810498e84ec2904408f805d31e0c54a
rjarrin <jarrin.ricardo@gmail.com>
2024-07-17 18:01:47 -0400

    Updated project to connect with Convex

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
-import Image from "next/image";
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "@/convex/_generated/api";
 import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const tasks = useQuery(api.tasks.get);
   return (
     <main className="flex flex-col items-center justify-between p-24">
       <h1>Hello World</h1>
-      <Button>Click me</Button>      
+      <Button>Click me</Button>
+      {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}      
     </main>
   );
 }
