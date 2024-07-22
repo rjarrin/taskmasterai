46168ccd05acb973c20e216c4868d35c40d1b752
rjarrin <jarrin.ricardo@gmail.com>
2024-07-22 14:22:33 -0400

    Updated Authentication security and insert into DB Implemented public-private key pairs and JWT signing for sercure authentication, able to insert user profile into convex database

--- app/providers.tsx
+++ app/providers.tsx
@@ -2,15 +2,41 @@
 
 import { ConvexProvider, ConvexReactClient } from "convex/react";
 import { Session } from "next-auth";
-import { SessionProvider } from "next-auth/react";
-import { ReactNode } from "react";
+import { SessionProvider, useSession } from "next-auth/react";
+import { ReactNode, useMemo } from "react";
 
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
+
+function getTokenFromSession(session: Session | null): string | null {
+  return session?.convexToken ?? null;
+}
+
+function useAuth() {
+  const {data: session, update} = useSession();
+  const convexToken = getTokenFromSession(session);
+  return useMemo(() => ({
+    isLoading: false,
+    isAuthenticated: session !== null,
+    fetchAccessToken: async ({
+      forceRefreshToken,
+    }: {
+      forceRefreshToken: boolean;
+    }) => {
+      if (forceRefreshToken) {
+        const session = await update();
+        return getTokenFromSession(session);
+      }
+      return convexToken;
+    },
+  }),
+  [JSON.stringify(session?.user)]
+  );
+}
 
 export default function Providers({ children, session }: { children: ReactNode, session: Session | null }) {
   return (
   <SessionProvider session={session}>
-    <ConvexProvider client={convex}>
+    <ConvexProvider client={convex} useAuth={useAuth}>
       {children}
     </ConvexProvider>
   </SessionProvider>
