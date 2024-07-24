0dffdc7f4c897c5cdaf95e98a4f2f2f6b2bd8fd7
rjarrin <jarrin.ricardo@gmail.com>
2024-07-24 15:26:42 -0400

    Updated loggedin page to use navigation sidebar

--- app/loggedin/page.tsx
+++ app/loggedin/page.tsx
@@ -6,19 +6,24 @@ import { Button } from "@/components/ui/button";
 import { signInAction } from "@/actions/auth-actions";
 import UserProfile from "@/components/navigation/userprofile";
 import Sidebar from "@/components/navigation/sidebar";
+import MobileNav from "@/components/navigation/mobilenav";
+import githubLogo from "@/public/github.svg";
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   const tasks = useQuery(api.tasks.get);
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <Sidebar />
-      <h1>Logged In</h1>
-      
-      <UserProfile />
-
-      <div>
-        <p>Tasks</p>
-          {tasks?.map(({ _id, text }) => <p key={_id}>{JSON.stringify(text)}</p>)} 
+      <div className="flex flex-col">
+        <MobileNav />
+        <main className="flex flex-1 flex-col gap-4 p-4 lg:px-8">
+          <div>
+            <p>Tasks</p>
+              {tasks?.map(({ _id, text }) => <p key={_id}>{JSON.stringify(text)}</p>)} 
+          </div>
+        </main>
       </div>
          
     </div>
