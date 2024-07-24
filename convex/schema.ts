c537496da872560529b06c730f02ca791b31d678
rjarrin <jarrin.ricardo@gmail.com>
2024-07-24 15:43:37 -0400

    Updated schema with task and project properties

--- convex/schema.ts
+++ convex/schema.ts
@@ -72,8 +72,45 @@ const authTables = {
 
 export default defineSchema({
   ...authTables,
-  tasks: defineTable({
+  todos: defineTable({
+    userId: v.id("users"),
+    projectId: v.id("projects"),
+    labelId: v.id("labels"),
+    taskName: v.string(),
+    description: v.optional(v.string()),
+    dueDate: v.number(),
+    priority: v.optional(v.float64()),
     isCompleted: v.boolean(),
-    text: v.string(),
+    embedding: v.optional(v.array(v.float64())),
+  }).vectorIndex("by_embedding", {
+    vectorField: "embedding",
+    dimensions: 1536,
+    filterFields: ["userId"],
+  }),
+  subTodos: defineTable({
+    userId: v.id("users"),
+    projectId: v.id("projects"),
+    labelId: v.id("labels"),
+    parentId: v.id("todos"),
+    taskName: v.string(),
+    description: v.optional(v.string()),
+    dueDate: v.number(),
+    priority: v.optional(v.float64()),
+    isCompleted: v.boolean(),
+    embedding: v.optional(v.array(v.float64())),
+  }).vectorIndex("by_embedding", {
+    vectorField: "embedding",
+    dimensions: 1536,
+    filterFields: ["userId"],
+  }),
+  labels: defineTable({
+    userId: v.union(v.id("users"), v.null()),
+    name: v.string(),
+    type: v.union(v.literal("user"), v.literal("system")),
+  }),
+  projects: defineTable({
+    userId: v.union(v.id("users"), v.null()),
+    name: v.string(),
+    type: v.union(v.literal("user"), v.literal("system")),
   }),
 });
