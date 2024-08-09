5e0c22ee761bbb7045067833a83cf6b8e541e4c0
rjarrin <jarrin.ricardo@gmail.com>
2024-08-09 13:59:34 -0400

    Updated Convex functions Functions include user authentication and project methods for projects, todos, and labels

--- convex/_generated/api.d.ts
+++ convex/_generated/api.d.ts
@@ -14,9 +14,13 @@ import type {
   FilterApi,
   FunctionReference,
 } from "convex/server";
+import type * as auth from "../auth.js";
 import type * as authAdapter from "../authAdapter.js";
 import type * as http from "../http.js";
+import type * as labels from "../labels.js";
+import type * as projects from "../projects.js";
 import type * as tasks from "../tasks.js";
+import type * as todos from "../todos.js";
 
 /**
  * A utility for referencing Convex functions in your app's API.
@@ -27,9 +31,13 @@ import type * as tasks from "../tasks.js";
  * ```
  */
 declare const fullApi: ApiFromModules<{
+  auth: typeof auth;
   authAdapter: typeof authAdapter;
   http: typeof http;
+  labels: typeof labels;
+  projects: typeof projects;
   tasks: typeof tasks;
+  todos: typeof todos;
 }>;
 export declare const api: FilterApi<
   typeof fullApi,
