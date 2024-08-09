de840f3306dc642c027fbabbeaecfa9e0c48cd0c
rjarrin <jarrin.ricardo@gmail.com>
2024-08-09 13:57:29 -0400

    Updated LoggedIn page layout with navigation items

--- app/loggedin/page.tsx
+++ app/loggedin/page.tsx
@@ -1,31 +1,17 @@
-"use client";
-
-import { useQuery } from "convex/react";
-import { api } from "@/convex/_generated/api";
-import { Button } from "@/components/ui/button";
-import { signInAction } from "@/actions/auth-actions";
-import UserProfile from "@/components/navigation/userprofile";
-import Sidebar from "@/components/navigation/sidebar";
 import MobileNav from "@/components/navigation/mobilenav";
-import githubLogo from "@/public/github.svg";
-import Image from "next/image";
-import Link from "next/link";
+import Sidebar from "@/components/navigation/sidebar";
+import TodoList from "@/components/todos/todolist";
 
 export default function Home() {
-  const tasks = useQuery(api.tasks.get);
-  return (
-    <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
-      <Sidebar />
-      <div className="flex flex-col">
-        <MobileNav />
-        <main className="flex flex-1 flex-col gap-4 p-4 lg:px-8">
-          <div>
-            <p>Tasks</p>
-              {tasks?.map(({ _id, text }) => <p key={_id}>{JSON.stringify(text)}</p>)} 
-          </div>
-        </main>
-      </div>
-         
-    </div>
-  );
+    return (
+        <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
+            <Sidebar />
+            <div className="flex flex-col">
+                <MobileNav />
+                <main className="flex flex-1 flex-col gap-4 p-4 lg:px-8">
+                    <TodoList />
+                </main>
+            </div>
+        </div>
+    );
 }
